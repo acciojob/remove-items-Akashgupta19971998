0d08d97052ef593474c6c0602ff3afa20fe0b753
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-04-13 16:11:45 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+// let selector=document.getElementById("colorSelect");
+let button=document.getElementById("btn");
+button.addEventListener("click", myFunction);
+function myFunction() {
+    var x = document.getElementById("mySelect");
+    x.remove(x.selectedIndex);
+  }
